30e6c9be20b15a93847df46ec1aa80d27d15c6c3
easesmith <tech@mavyax.com>
2026-07-26 02:51:36 +0530

    vercel fix

--- src/connection/express_connection.js
+++ src/connection/express_connection.js
@@ -5,10 +5,30 @@ const { mountRouteGroups } = require("./routeRegistry");
 const { attachCurrentUser } = require("../middlewares/auth.middleware");
 const { notFoundHandler, errorHandler } = require("../middlewares/error.middleware");
 
+const allowedOrigins = new Set([
+  "http://localhost:3000",
+  "https://nimbusos.easesmith.com",
+]);
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.has(origin)) {
+      return callback(null, true);
+    }
+
+    return callback(new Error(`CORS origin not allowed: ${origin}`));
+  },
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "x-demo-user-id"],
+  credentials: false,
+  optionsSuccessStatus: 204,
+};
+
 const createExpressApp = (options = {}) => {
   const app = express();
 
-  app.use(cors());
+  app.use(cors(corsOptions));
+  app.options(/.*/, cors(corsOptions));
   app.use(express.json());
   app.use(attachCurrentUser);
 
